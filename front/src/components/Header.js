ec7eaa5d59b3e41b25181e2e049a933f3f9ec06f
sorashin <naka52shin@gmail.com>
2021-04-18 11:39:09 +0900

    add Header

---
+++ front/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react"
+import styled from 'styled-components'
+import logo from '../images/logo.png'
+
+export default function Header({ children }) {
+    return (
+        <Wrap>
+            <Container>
+                <Logo><img src={logo} /></Logo>
+                <Hint>初めての方へ</Hint>
+            </Container>
+        </Wrap>
+    )
+}
+
+const Wrap = styled.div`
+    position:fixed;
+    width:100%;
+    height:56px;
+    background:#ffffff;
+    border-bottom:solid 1px #eeeeee;
+`
+
+const Container = styled.div`
+    display:flex;
+    justify-content: space-between;
+    width:100%;
+    height:100%;
+    max-width:960px;
+    margin:0 auto;
+`
+const Logo = styled.a`
+    img{
+        height:100%;
+        width:auto;
+    }
+`
+
+const Hint = styled.a`
+
+`
