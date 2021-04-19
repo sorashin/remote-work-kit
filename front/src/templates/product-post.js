515cdeda763eb6a1694bc4f18f2551ae13b34488
sorashin <naka52shin@gmail.com>
2021-04-19 11:18:59 +0900

    change Layout path

--- front/src/templates/product-post.js
+++ front/src/templates/product-post.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import ARLogo from '../images/AR.png'
 import styled from 'styled-components'
